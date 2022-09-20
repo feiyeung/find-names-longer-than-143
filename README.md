df2fd9163ae0dab5036de25f6a31ef435e5a4e71
Feiyang Xue <hi@fxue.dev>
2022-09-20 17:40:26 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # find-names-longer-than-143
-Scan filesystem and locate filename or dirname that's more than 143 bytes
+
+Scan filesystem and locate filename or dirname that's more than 143 bytes (not to be confused with 143 characters). Used for making my files compatible with eCryptFS.
+
+Why do I not just use some shell command or python, that might seem more readable and one-line-able? Well I tried few, but i still run into issues with non-ASCII or non-UNICODE characters, such that some tools might not account for their actual number of bytes correctly.
+
+## Build
+
+    gcc -o whatever names_longer_than_ecryptfs.c
+
+## Usage Example
+
+To scan a dir and list only entries whose names are more than 143 bytes, while saving to a file
+
+    ./whatever <path(s) to scan> 2>/dev/null | tee gt143.txt
+
+To scan and list everything (regardless of longer than 143 or not), while saving to a file:
+
+    ./whatever <path(s) to scan> 2>&1 | tee everything.txt
+
+## Output format
+
+    <number of bytes for the name> <whitespace> <quote> <fullpath> <quote>
---
+++ names_longer_than_ecryptfs.c
@@ -0,0 +1,56 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define PATH_MAX 4096
+#define ECRYPTFS_FILENAME_MAX 143
+
+int walkdir(const char *dirname)
+{
+    DIR *dir;
+    struct dirent *entry;
+    char path[PATH_MAX];
+    unsigned int name_len;
+    FILE* outs;
+
+    if (!(dir = opendir(dirname)))
+        return 1;
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+                continue;
+
+        snprintf(path, sizeof(path), "%s/%s", dirname, entry->d_name);
+        name_len = strlen(entry->d_name);
+        outs = (name_len <= ECRYPTFS_FILENAME_MAX) ? stderr : stdout ;
+        fprintf(outs, "%3lu \"%s\"\n", strlen(entry->d_name), path);
+
+        if (entry->d_type == DT_DIR) {
+            walkdir(path);
+        }
+    }
+    closedir(dir);
+    return 0;
+}
+
+int main(int argc, char** argv) {
+    char* path;
+    if (argc < 2) {
+        path=getenv("PWD");
+        return walkdir(path);
+    } else {
+        int i, ret=0;
+        for (i = 1; i < argc; ++i) {
+            ret = walkdir(argv[i]);
+            if (!ret) return ret;
+        }
+    }
+
+    // fprintf(stderr, "launching from path \"%s\"\n\n", path);
+
+    return 0;
+
+}
